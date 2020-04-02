751077686d9b4eef6beceb07cc8c88758bd22a1e
marcoshung <mhung927@gmail.com>
2020-04-01 20:01:46 -0700

    final

--- Mancala/src/Mancala.java
+++ Mancala/src/Mancala.java
@@ -9,6 +9,7 @@ public class Mancala {
 		BoardPanel bp = new BoardPanel(b);
 		
 		frame.add(bp);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    frame.setPreferredSize(new Dimension(1000,1000));
 	    frame.pack();
 		frame.setVisible(true);
