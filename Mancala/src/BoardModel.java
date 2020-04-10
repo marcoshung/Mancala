e6882ef193f85306354be84a07471f631c648d3f
marcoshung <mhung927@gmail.com>
2020-04-10 14:28:44 -0700

    test

--- Mancala/src/BoardModel.java
+++ Mancala/src/BoardModel.java
@@ -8,10 +8,14 @@ public class BoardModel {
 	
 	private Stack<GameState> history;
 	private ArrayList<ChangeListener> listeners;
-	
+	private GameManager gameManager;
+	private final int INTIAL_MARBLES_IN_PITS = 3;
 	public BoardModel() {
 		history = new Stack<GameState>();
 		listeners = new ArrayList<ChangeListener>();
+		gameManager = new GameManager();
+		gameManager.set_stoneInPit(gameManager.load_current_GameState(), INTIAL_MARBLES_IN_PITS);
+		history.push(gameManager.load_current_GameState());
 	}
 	
 	public void attach(ChangeListener c) {
@@ -24,25 +28,15 @@ public class BoardModel {
 	
 	public void update(String cell) {
 		char player = cell.charAt(0);
-		int pitNum = cell.charAt(1);
+		int pitNum = Integer.parseInt(cell.substring(1));
 		
 		int pitLocation = pitNum - 1;
 		if(player == 'b' || player == 'B'){
 			pitLocation += 6;
 		}
 		
-		ArrayList<Integer> currentState = getState().getPits();
-		int marbles = currentState.get(pitLocation);
-		currentState.set(pitLocation, 0);
-		int position = pitLocation + 1;
-		for(int i = 0; i < marbles; i++) {
-			if(position > 12) {
-				position = 0;
-			}
-			currentState.set(position, currentState.get(position) + 1);
-			position++;
-		}
-		
+		gameManager.playGame(gameManager.load_current_GameState(), pitLocation);
+		history.push(gameManager.load_current_GameState());
 		for(ChangeListener l : this.listeners) {
 			l.stateChanged(new ChangeEvent(this));
 		}
@@ -50,6 +44,15 @@ public class BoardModel {
 	
 	public void undo() {
 		history.pop();
+		GameState prevState = history.peek();
+		gameManager.set_stoneInPit(prevState, 0);
 	}
 	
+	public GameManager getGameManager() {
+		return this.gameManager;
+	}
+	
+	public GameState getCurrentState() {
+		return history.peek();
+	}
 }
